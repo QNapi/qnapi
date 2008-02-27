435a5d8c2f389441bb1699e904d3fd4a9933325b
Piotr Krzemiński <pio.krzeminski@gmail.com>
2008-02-27 13:20:35 +0000

    Obsluga wyboru przez uzytkownika czy uzywany jest styl BrushedMetal (Mac OS X)

--- src/frmabout.cpp
+++ src/frmabout.cpp
@@ -8,8 +8,9 @@
 frmAbout::frmAbout(QWidget * parent, Qt::WFlags f) : QDialog(parent, f)
 {
 	ui.setupUi(this);
+
 #ifdef Q_WS_MAC
-	setAttribute(Qt::WA_MacBrushedMetal, true);
+	setAttribute(Qt::WA_MacBrushedMetal, GlobalConfig().useBrushedMetal());
 #endif
 	setAttribute(Qt::WA_QuitOnClose, false);
 	ui.lbTitle->setText(QString("QNapi ") + QNAPI_VERSION);

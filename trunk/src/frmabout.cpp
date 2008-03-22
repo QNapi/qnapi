f44029d0244fef98e67e7876d7c2175d8bc8ff92
krzemin <krzemin@9fe90444-c646-0410-9a6b-ea38b918db2a>
2008-03-22 16:09:35 +0000

    Dodane podziekowania + resizeEvent wywalony na korzysc layoutow

--- trunk/src/frmabout.cpp
+++ trunk/src/frmabout.cpp
@@ -5,6 +5,8 @@
 
 #include "frmabout.h"
 
+
+
 frmAbout::frmAbout(QWidget * parent, Qt::WFlags f) : QDialog(parent, f)
 {
 	ui.setupUi(this);
@@ -14,7 +16,7 @@ frmAbout::frmAbout(QWidget * parent, Qt::WFlags f) : QDialog(parent, f)
 #endif
 	setAttribute(Qt::WA_QuitOnClose, false);
 	ui.lbTitle->setText(QString("QNapi ") + QNAPI_VERSION);
-	
+
 const QString aboutText = tr(
 "<html><body><p>QNapi jest nieoficjalnym, wolnym klonem programu NAPI-PROJEKT, "
 "napisanym z myślą o użytkownikach systemów Linux, Mac OS X oraz innych, pod które "
@@ -23,17 +25,20 @@ const QString aboutText = tr(
 "Krzemin &lt;<a href=\"mailto:pkrzemin@o2.pl?subject=QNapi\">pkrzemin@o2.pl</a>&gt;"
 "</p><p><b>WWW</b>: <a href=\"http://krzemin.iglu.cz/qnapi\">http://krzemin.iglu.cz/qnapi</a>"
 "</p></body></html>");
+const QString thanksText = tr(
+"<html><body><p>Podziękowania należą się wszystkim osobom, które aktywnie wsparły rozwój "
+"QNapi, a w szczególności:<ul><li>adrian5632 &lt;<a href=\"mailto:adrian5632@gmail.com\">"
+"adrian5632@gmail.com</a>&gt; - opiekun wersji na Mac OS X, liczne patche i poprawki</li>"
+"<li>Rafał Wójcik &lt;<a href=\"mailto:rafal.wojcik@gmail.com\">rafal.wojcik@gmail.com</a>&gt;"
+" - port dla Windows wraz z instalatorem</li></ul></p><p>A także paczkującym dla różnych "
+"dystrybucji:</p><p>obi_gl, Jakub Schmidtke, Jan Stępień, Łukasz Jagiełło</p><p>"
+"...oraz wszystkim tym, o których zapomniałem ;)</p></body></html>");
 
 	ui.tbAbout->setText(aboutText);
+	ui.tbThanks->setText(thanksText);
 
 	// workaround dla compiza?
 	move((QApplication::desktop()->width() - width()) / 2, 
 		(QApplication::desktop()->height() - height()) / 2);
 }
 
-void frmAbout::resizeEvent(QResizeEvent *resize)
-{
-	int b = 2;
-	ui.horizontalLayout->setGeometry(QRect(b, b, resize->size().width() - b*2,
-										resize->size().height() - b*2));
-}

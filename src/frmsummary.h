d5623d10407693c8435377a913860392ee2b269a
Piotr Krzemiński <pio.krzeminski@gmail.com>
2008-04-05 12:31:51 +0000

    Dialog obslugujacy bardziej rozbudowane podsumowanie pobierania napisow

---
+++ src/frmsummary.h
@@ -0,0 +1,30 @@
+/* QNapi
+ * Autor: Krzemin <pkrzemin@o2.pl>
+ * Ten program jest rozprowadzany na licencji GNU General Public License.
+ */
+
+#ifndef __FRMSUMMARY__H__
+#define __FRMSUMMARY__H__
+
+#include <QDesktopWidget>
+#include <QResizeEvent>
+#include <QListWidget>
+#include <QFileInfo>
+
+#include "ui_frmsummary.h"
+
+class frmSummary: public QDialog
+{
+Q_OBJECT
+	public:
+		frmSummary(QWidget *parent = 0, Qt::WFlags f = 0);
+		~frmSummary() {};
+
+		void setFileList(const QStringList & list);
+		void setFailedCount(unsigned int failed);
+
+	private:
+		Ui::frmSummary ui;
+};
+
+#endif

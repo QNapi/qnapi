03896042e59a2faebe3d79065a12260d68885091
Piotr Krzemiński <pio.krzeminski@gmail.com>
2008-04-10 13:08:31 +0000

    Uaktualniony komentarz w naglowku - notka na temat licencji GPL v3

--- src/synchttp.h
+++ src/synchttp.h
@@ -1,8 +1,26 @@
-/***************************************************************************
- *   Copyright (C) 2005 by Iulian M                                        *
- *   eti@erata.net                                                         *
- *   syncRequest added by Krzemin <pkrzemin@o2.pl>                         *
- ***************************************************************************/
+/*************************************************************************
+
+    Copyright (C) 2005 by Iulian M
+    eti@erata.net
+
+    syncRequest added by Krzemin
+    Copyright (C) 2008 Krzemin <pkrzemin@o2.pl>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*************************************************************************/
+
 #ifndef ETKSYNCHTTP_H
 #define ETKSYNCHTTP_H
 

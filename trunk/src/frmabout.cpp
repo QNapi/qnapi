6d3d791a21de40a83974e11b9ee8b00227cf42ba
krzemin <krzemin@9fe90444-c646-0410-9a6b-ea38b918db2a>
2008-04-12 14:43:33 +0000

    Naglowek w komentarzu zmieniony z GPL3 na GPL 2 or later

--- trunk/src/frmabout.cpp
+++ trunk/src/frmabout.cpp
@@ -1,22 +1,16 @@
-/*************************************************************************
-
-    QNapi
-    Copyright (C) 2008 Krzemin <pkrzemin@o2.pl>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*************************************************************************/
+/*****************************************************************************
+** QNapi
+** Copyright (C) 2008 Krzemin <pkrzemin@o2.pl>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+*****************************************************************************/
 
 #include "frmabout.h"
 
@@ -34,10 +28,11 @@ const QString aboutText = tr(
 "<html><body><p>QNapi jest nieoficjalnym, wolnym klonem programu NAPI-PROJEKT, "
 "napisanym z myślą o użytkownikach systemów Linux, Mac OS X oraz innych, pod które "
 "oryginalny klient NAPI nie jest dostępny.</p><p></p><p><b>Ten program rozprowadzany "
-"jest na licencji GNU General Public License.</b></p><p></p><p><b>Autor</b>: "
-"Krzemin &lt;<a href=\"mailto:pkrzemin@o2.pl?subject=QNapi\">pkrzemin@o2.pl</a>&gt;"
-"</p><p><b>Prawa autorskie:</b> Copyright (C) 2008 Krzemin</p><p><b>WWW</b>: "
-"<a href=\"http://krzemin.iglu.cz/qnapi\">http://krzemin.iglu.cz/qnapi</a></p></body></html>");
+"jest na licencji GNU General Public License w wersji 2. lub każdej nowszej.</b></p>"
+"<p></p><p><b>Autor</b>: Krzemin &lt;<a href=\"mailto:pkrzemin@o2.pl?subject=QNapi\">"
+"pkrzemin@o2.pl</a>&gt;</p><p><b>Prawa autorskie:</b> Copyright (C) 2008 Krzemin</p>"
+"<p><b>WWW</b>:  <a href=\"http://krzemin.iglu.cz/qnapi\">http://krzemin.iglu.cz/qnapi</a></p>"
+"</body></html>");
 const QString thanksText = tr(
 "<html><body><p>Podziękowania należą się wszystkim osobom, które aktywnie wsparły rozwój "
 "QNapi, a w szczególności:<ul><li>adrian5632 &lt;<a href=\"mailto:adrian5632@gmail.com\">"

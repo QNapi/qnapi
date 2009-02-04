9924b0d813f6f09d419f14f5cfcbb211452805d2
Piotr Krzemiński <pio.krzeminski@gmail.com>
2009-02-04 23:40:23 +0000

    Drobne zmiany, gl. kosmetyka

--- src/qnapiprojektengine.h
+++ src/qnapiprojektengine.h
@@ -30,11 +30,13 @@
 #include "synchttp.h"
 #include "movieinfo.h"
 
+#include "qnapilanguage.h"
+
 const unsigned long NAPI_10MB = 10485760;
 
 const QString napiZipPassword = "iBlm8NTigvru0Jr0";
 const QString napiDownloadUrlTpl= "http://www.napiprojekt.pl/unit_napisy/dl.php"
-				"?l=%1&f=%2&t=%3&v=other&kolejka=false&napios=Linux&nick=%4&pass=%5";
+				"?l=%1&f=%2&t=%3&v=other&kolejka=false&nick=%4&pass=%5&napios=%6";
 const QString napiCheckUserUrlTpl = "http://www.napiprojekt.pl/users_check.php?nick=%1&pswd=%2";
 const QString napiUploadUrlTpl = "http://www.napiprojekt.pl/unit_napisy/upload.php"
 				"?m_length=%1&m_resolution=%2x%3&m_fps=%4&m_hash=%5&m_filesize=%6";

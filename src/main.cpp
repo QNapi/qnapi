9c45e0cc70b66d7ae1568d0fadb2f1c6c085ea4b
Piotr Krzemiński <pio.krzeminski@gmail.com>
2009-02-16 20:04:28 +0000

    Uaktualniona notka Copyright

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /*****************************************************************************
 ** QNapi
-** Copyright (C) 2008 Krzemin <pkrzemin@o2.pl>
+** 
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by

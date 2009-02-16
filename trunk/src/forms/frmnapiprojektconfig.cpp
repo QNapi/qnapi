4fa434bb6003665a8f7e0693baf5cab52baf0909
krzemin <krzemin@9fe90444-c646-0410-9a6b-ea38b918db2a>
2009-02-16 20:04:28 +0000

    Uaktualniona notka Copyright

--- trunk/src/forms/frmnapiprojektconfig.cpp
+++ trunk/src/forms/frmnapiprojektconfig.cpp
@@ -1,6 +1,6 @@
 /*****************************************************************************
 ** QNapi
-** Copyright (C) 2008 Krzemin <pkrzemin@o2.pl>
+** Copyright (C) 2008-2009 Krzemin <pkrzemin@o2.pl>
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by

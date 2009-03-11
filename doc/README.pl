6f302ee07a84e444a55a30e7f43b2f365f8dac70
Piotr Krzemiński <pio.krzeminski@gmail.com>
2009-03-11 14:18:37 +0000

    Uaktualniona dokumentacja m.in. o opis jak zintegrowac skanowanie katalogow z menedzerami plikow + kosmetyka

--- doc/README.pl
+++ doc/README.pl
@@ -27,7 +27,7 @@ http://krzemin.iglu.cz/qnapi
 
 Jesli pakiety pod Twoja dystrybucje nie sa dostepne, mozesz samodzielnie
 skompilowac program ze zrodel. Do tego potrzebne beda biblioteki deweloperskie
-Qt4 (w niektorych dystrucjach oznaczone jako libqt4-dev lub podobnie)
+Qt4 (w niektorych dystrucjach oznaczone jako libqt4-dev, qt4-dev lub podobnie)
 i program 7zip (dostepny w pakiecie p7zip-full).
 
 Pobierz i rozpakuj archiwum .tar.gz z kodem zrodlowym programu:
@@ -41,7 +41,8 @@ $ cd qnapi-wersja
 Aby skompilowac program, wykonaj po kolei polecenia:
 
 $ qmake
-(lub qmake-qt4)
+lub qmake-qt4; upewnij sie, ze uzyta wersja qmake (qmake --version) przeznaczona
+jest dla Qt4
 
 $ make
 
@@ -91,17 +92,20 @@ prawym przyciskiem myszy na plik z filmem, w menu pojawila sie opcja "Pobierz
 napisy do filmu z QNapi", wykonaj ponizsze instrukcje, adekwatne do uzywanego
 przez Ciebie programu.
 
-KDE (Dolphin/Konqueror)
------------------------
-UWAGA: Od wersji 0.1.4 QNapi powinien automatycznie integrowac sie z programami
-Dolphin oraz Konqueror. Jesli automatyczna integracja nie bedzie dzialala,
-mozesz skorzystac z alternatywnej metody:
+W niektorych dystrybucjach powinny znajdowac sie pakiety qnapi-kde3, qnapi-kde4,
+qnapi-gnome itd., ktore automatycznie integruja program z uzywanym
+srodowiskiem/menedzerem plikow. Jesli w Twojej dystrybucji nie ma takich
+pakietow, skorzystaj z opisu przedstawionego ponizej.
 
-Bedzie potrzebny plik qnapi-download.dekstop, ktory mozesz znalezc w podkatalogu
-doc/ archiwum zrodlowego, w /usr/share/doc/qnapi (po instalacji programu) lub
-na stronie www http://krzemin.iglu.cz/qnapi.
 
-Nalezy skopiowac plik do katalogow:
+KDE 3.x (Dolphin/Konqueror)
+---------------------------
+
+Beda potrzebne pliki qnapi-download.desktop i qnapi-scan.desktop, ktore
+znajdziesz w podkatalogu doc/ archiwum zrodlowego lub w /usr/share/doc/qnapi
+(po instalacji programu).
+
+Nalezy skopiowac pliki do katalogow:
  *  ~/.kde/share/apps/konqueror/servicemenus/
  *  ~/.kde/share/apps/d3lphin/servicemenus/
  *  ~/.kde/share/apps/dolphin/servicemenus/
@@ -111,45 +115,57 @@ Lub globalnie (wymaga uprawnien administratora):
  *  /usr/share/apps/d3lphin/servicemenus/
  *  /usr/share/apps/dolphin/servicemenus/
 
-Dla KDE4:
 
-* ~/.kde4/share/kde4/services/ServiceMenus/
+Powyzsze sciezki do katalogow moga sie minimalnie roznic, w zaleznosci od
+uzywanej dystrybucji.
 
-Globalnie dla KDE4:
- * /usr/lib/kde4/share/kde4/services/ServiceMenus/
 
-Powyzsze katalogi moga byc nieco inne w Twojej dystrybucji, w zaleznosci od
-tego, gdzie zostalo zainstalowane KDE.
+KDE4
+----
+
+Polecenie:
+
+$ kde4-config --path services
+
+zwroci liste katalogow oddzielonych dwukropkiem:
+
+/home/user/.kde4/share/kde4/services/:/usr/share/kde4/services/
+
+Pliki qnapi-download.desktop i qnapi-scan.desktop nalezy skopiowac do katalogu:
+
+/home/user/.kde4/share/kde4/services/ServiceMenus
+
+lub:
+
+/usr/share/kde4/services/ServiceMenus
+
+(globalnie, wymaga uprawnien administratora)
+
 
 Nautilus (GNOME)
 ----------------
-UWAGA: Od wersji 0.1.5 QNapi powinien automatycznie integrowac sie z Nautilusem
-po zainstalowaniu pakietu qnapi-gnome, wlasciwego dla Twojej dystrybucji oraz po
-ponownym uruchomieniu systemu (lub przeladowaniu demona gconfd).
+Potrzebny bedzie zainstalowany pakiet nautilus-actions oraz pliki
+qnapi-download.schemas i qnapi-scan.schemas, ktore mozesz znalezc w podkatalogu
+doc/ archiwum zrodlowego lub w /usr/share/doc/qnapi (po instalacji programu).
 
-Jesli automatyczna integracja nie zadziala, badz nie chcesz (nie mozesz)
-zainstalowac dodatkowego pakietu, mozesz skorzystac z alternatywnego spososbu na
-integracje QNapi z Nautilusem:
-
-Potrzebny bedzie zainstalowany pakiet nautilus-actions oraz plik
-qnapi-download.schemas, ktory mozesz znalezc w podkatalogu doc/ archiwum
-zrodlowego, w /usr/share/doc/qnapi (po instalacji programu) lub na stronie www
-http://krzemin.iglu.cz/qnapi.
-
-Wystarczy skopiowac plik qnapi-download.schemas do katalogu
-/usr/share/gconf/schemas/ i wykonac polecenie:
+Wystarczy skopiowac pliki do katalogu
+/usr/share/gconf/schemas/ i wykonac polecenia:
 
 $ sudo gconf-schemas --register /usr/share/gconf/schemas/qnapi-download.schemas
+$ sudo gconf-schemas --register /usr/share/gconf/schemas/qnapi-scan.schemas
 
-Jesli to by nie zadzialalo, mozna sprobowac zaimportowac plik
-qnapi-download.schemas narzedziem nautilus-actions-config.
+Jesli podana wyzej metoda nie zadziala, mozesz sprobowac zaimportowac pliki
+qnapi-download.schemas i qnapi-scan.schemas narzedziem nautilus-actions-config:
+
+$ nautilus-actions-config
+
 
 Thunar (XFCE)
 -------------
 Integracja z Thunarem wymaga uruchomienia programu i wybrania opcji
 Edycja -> Konfiguruj akcje...
 
-Na zakladce "Podstawowe" nalezy ustawic:
+Dodajemy nową akcję przyciskiem "+". Na zakladce "Podstawowe" nalezy ustawic:
 
 Nazwa: "Pobierz napisy do filmu z QNapi"
 Opis: (dowolny)
@@ -158,6 +174,10 @@ Polecenie: qnapi %F
 Mozna wybrac ikone z pliku graficznego (np. /usr/share/icons/qnapi-48.png).
 Na zakladce "Warunki pokazywania" nalezy zaznaczyc tylko "Filmy wideo".
 Potem OK i gotowe.
+
+Aby miec mozliwosc skanowania katalogow z poziomu Thunara, dodajemy nowa akcje.
+Nazwa i opis dowolna, polecenie ustawiamy na "qnapi %f", natomaist na zakladce
+"Warunki pokazywania" zaznaczamy tylko "Katalogi".
 
 
 Raportowanie bledow

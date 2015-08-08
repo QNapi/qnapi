3c1f9b5e362e4f8fab7e5e478d5e0f76f15a2be6
Piotr Krzemiński <pio.krzeminski@gmail.com>
2015-08-08 21:45:00 +0200

    bump version number to 0.1.8, update changelog

--- src/version.h
+++ src/version.h
@@ -16,7 +16,7 @@
 #define __VERSION__H__
 
 #define QNAPI_VERSION "0.1.8"
-#define QNAPI_DISPLAYABLE_VERSION "0.1.8-snapshot"
+#define QNAPI_DISPLAYABLE_VERSION "0.1.8"
 #define QNAPI_URL "http://qnapi.github.io"
 
 #endif

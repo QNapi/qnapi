60a51754598aeb7a1d8afc013e6ef56827f3ce0b
Piotr Krzemiński <pio.krzeminski@gmail.com>
2015-08-18 21:06:49 +0200

    lifted definition of subtitle entry (introduction of SubToken concept)

--- src/subconvert/subfile.h
+++ src/subconvert/subfile.h
@@ -4,14 +4,33 @@
 #include <QStringList>
 #include <QVector>
 
+enum SubTokenType
+{
+    STT_WS = 0,
+    STT_WORD = 1,
+    STT_NEWLINE = 2,
+    STT_BOLD = 3,
+    STT_BOLD_END = 4,
+    STT_ITALIC = 5,
+    STT_ITALIC_END = 6,
+    STT_UNDERLINE = 7,
+    STT_UNDERLINE_END = 8,
+    STT_FONTCOLOR = 9,
+    STT_FONTCOLOR_END = 10
+};
+
+struct SubToken {
+    SubTokenType type;
+    QString payload;
+};
+
 struct SubEntry
 {
     long frameStart, frameStop;
     // if time-based, they represent frames as milliseconds
     // if frame-based, they represent exact frame numbers
-    QStringList contentLines;
-    QVector<int> boldLines, italicLines;
-    QVector<std::pair<int,QString> > colorLines;
+
+    QVector<SubToken> tokens;
 };
 
 struct SubFile

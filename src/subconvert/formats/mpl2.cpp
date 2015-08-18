263b7198674c7ed6091f249c5057e1d15f0e95eb
Piotr Krzemiński <pio.krzeminski@gmail.com>
2015-08-18 21:35:19 +0200

    encoding token stream in MPL2 format

--- src/subconvert/formats/mpl2.cpp
+++ src/subconvert/formats/mpl2.cpp
@@ -13,6 +13,38 @@ SubFile MPL2SubtitleFormat::decode(const QStringList &lines)
 
 QStringList MPL2SubtitleFormat::encode(const SubFile & subFile)
 {
-    QStringList out;
-    return out;
+    QStringList lines;
+    foreach(const SubEntry & entry, subFile.entries)
+    {
+        lines.append(encodeEntry(entry));
+    }
+    return lines;
+}
+
+QString MPL2SubtitleFormat::encodeEntry(const SubEntry & entry)
+{
+    QString tokensBuff;
+    foreach(const SubToken & token, entry.tokens)
+    {
+        tokensBuff += encodeToken(token);
+    }
+    return QString("[%1][%2]%3\r\n")
+            .arg(entry.frameStart / 100)
+            .arg(entry.frameStop / 100)
+            .arg(tokensBuff);
+}
+
+QString MPL2SubtitleFormat::encodeToken(const SubToken & entry)
+{
+    switch(entry.type)
+    {
+    case STT_WS: return " ";
+    case STT_WORD: return entry.payload;
+    case STT_NEWLINE: return "|";
+    case STT_BOLD: return "{y:b}";
+    case STT_ITALIC: return "{y:i}";
+    case STT_UNDERLINE: return "{y:u}";
+    case STT_FONTCOLOR: return QString("{c:%1}").arg(entry.payload);
+    default: return "";
+    }
 }

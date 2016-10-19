9b1a0da59d417ea8c81528c14c49ae749bee9217
Piotr Krzemiński <pio.krzeminski@gmail.com>
2016-10-19 18:05:22 +0200

    no emptyline

--- scripts/version.bat
+++ scripts/version.bat
@@ -3,5 +3,4 @@ set VERSION_FILE=src\version.h
 cat %VERSION_FILE% | grep QNAPI_DISPLAYABLE_VERSION > tmp.txt
 set /p VERSION_LINE=<tmp.txt
 rem tmp.txt
-echo %VERSION_LINE:~35,-1%
-
+echo | set /p dummy=%VERSION_LINE:~35,-1%

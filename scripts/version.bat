5e8adeafbec9020ebf4ef865a1d52088678ddcc4
Piotr Krzemiński <pio.krzeminski@gmail.com>
2016-10-19 19:21:24 +0200

    Appveyor portable build (#89)
    appveyor portable zip building

---
+++ scripts/version.bat
@@ -0,0 +1,6 @@
+@echo off
+set VERSION_FILE=src\version.h
+cat %VERSION_FILE% | grep QNAPI_DISPLAYABLE_VERSION > tmp.txt
+set /p VERSION_LINE=<tmp.txt
+rem tmp.txt
+echo | set /p dummy=%VERSION_LINE:~35,-1%

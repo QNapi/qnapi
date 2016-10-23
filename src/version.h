1061f1476096bd49020885bf21c6b389d7303e77
Piotr Krzemiński <pio.krzeminski@gmail.com>
2016-10-23 09:58:43 +0200

    bump version to 0.2.3-snapshot

--- src/version.h
+++ src/version.h
@@ -15,8 +15,8 @@
 #ifndef __VERSION__H__
 #define __VERSION__H__
 
-#define QNAPI_VERSION "0.2.2"
-#define QNAPI_DISPLAYABLE_VERSION "0.2.2"
+#define QNAPI_VERSION "0.2.3"
+#define QNAPI_DISPLAYABLE_VERSION "0.2.3-snapshot"
 #define QNAPI_URL "http://qnapi.github.io"
 
 #endif

b8b54348bd360b5fabb279920744ed61fbcf0ddf
Piotr Krzemiński <pio.krzeminski@gmail.com>
2017-06-06 17:31:13 +0200

    get rid of cat

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 VERSION_FILE=libqnapi/src/version.h
-cat $VERSION_FILE | grep QNAPI_DISPLAYABLE_VERSION | awk '{gsub(/"/, "", $3); print $3}'
+(grep QNAPI_DISPLAYABLE_VERSION | awk '{gsub(/"/, "", $3); print $3}') < $VERSION_FILE
+
